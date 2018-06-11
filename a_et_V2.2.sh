07b85ce97cb9aec8983bf56ba9e8f2c059bd07db
badubko <badubko@gmail.com>
2018-06-11 00:00:52 -0300

    Agregado a_et

---
+++ a_et_V2.2.sh
@@ -0,0 +1,1 @@
+cat carga_parametros_V1.96.sh crea_listado_repositorio_V2.1.sh listar_modulos_V1.7.sh verif_dirs_y_files_V1.96.sh carga_patrones_V2.0.sh obtener_extension_y_tipo_V2.1.sh buscar_en_repositorio_V2.0.sh NC_en_mem_V1.95.sh generar_reporte_V2.0.sh inicializar_contadores_V2.0.sh a_et_main_V2.2.sh
