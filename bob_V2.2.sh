5a82a94673d8f6acc35f3d8beaff3a0bee442f33
badubko <badubko@gmail.com>
2018-06-11 16:33:51 -0300

    Cambiado las versiones de los modulos a V2.2

--- bob_V2.2.sh
+++ bob_V2.2.sh
@@ -3,28 +3,28 @@
 
 declare -A LISTA_FUNCIONES MAIN OUT_FILE
 
-LISTA_FUNCIONES[a_et]="carga_parametros_V1.96.sh \
-                       crea_listado_repositorio_V2.1.sh \
-                       listar_modulos_V1.7.sh \
-						verif_dirs_y_files_V1.96.sh \
-						carga_patrones_V2.0.sh \
-						obtener_extension_y_tipo_V2.1.sh \
-						buscar_en_repositorio_V2.0.sh \
-						NC_en_mem_V1.95.sh \
-						generar_reporte_V2.0.sh \
-						inicializar_contadores_V2.0.sh"
+LISTA_FUNCIONES[a_et]="carga_parametros_V2.2.sh \
+                       crea_listado_repositorio_V2.2.sh \
+                       listar_modulos_V2.2.sh \
+						verif_dirs_y_files_V2.2.sh \
+						carga_patrones_V2.2.sh \
+						obtener_extension_y_tipo_V2.2.sh \
+						buscar_en_repositorio_V2.2.sh \
+						NC_en_mem_V2.2.sh \
+						generar_reporte_V2.2.sh \
+						inicializar_contadores_V2.2.sh"
 						
-LISTA_FUNCIONES[a_nc]="carga_parametros_V1.96.sh \
-						crea_listado_repositorio_V2.1.sh \
-						listar_modulos_V1.7.sh \
-						verif_dirs_y_files_V1.96.sh \
-						carga_patrones_V2.0.sh \
-						obtener_extension_y_tipo_V2.1.sh \
-						buscar_en_repositorio_V2.0.sh \
-						NC_en_mem_V1.95.sh \
-						generar_reporte_V2.0.sh \
-						reporte_t_parc_V1.0.sh \
-						inicializar_contadores_V2.0.sh"
+LISTA_FUNCIONES[a_nc]="carga_parametros_V2.2.sh \
+						crea_listado_repositorio_V2.2.sh \
+						listar_modulos_V2.2.sh \
+						verif_dirs_y_files_V2.2.sh \
+						carga_patrones_V2.2.sh \
+						obtener_extension_y_tipo_V2.2.sh \
+						buscar_en_repositorio_V2.2.sh \
+						NC_en_mem_V2.2.sh \
+						generar_reporte_V2.2.sh \
+						reporte_t_parc_V2.2.sh \
+						inicializar_contadores_V2.2.sh"
 MAIN[a_et]=a_et_main_V2.2.sh
 MAIN[a_nc]=a_nc_main_V2.2.sh
 OUT_FILE[a_et]="a_et_V2.2.sh"
