807ada557f2567b3d3d645f2d3216f0f072a28d8
badubko <badubko@gmail.com>
2018-08-06 22:49:41 -0300

    Las lineas de prueba hechas comentario

--- a_nc_main_V2.2.sh
+++ a_nc_main_V2.2.sh
@@ -154,9 +154,9 @@ then
 	exit
 fi
 
-  echo "${PARMS_A_INCLUIR}"
-  echo "${INSTALL_DIR}/${PARMS_A_INCLUIR}"
-  exit
+#  echo "${PARMS_A_INCLUIR}"
+#  echo "${INSTALL_DIR}/${PARMS_A_INCLUIR}"
+#  exit
 
 source "${INSTALL_DIR}/${PARMS_A_INCLUIR}"
 #-------------------------------------------------------------------------------
